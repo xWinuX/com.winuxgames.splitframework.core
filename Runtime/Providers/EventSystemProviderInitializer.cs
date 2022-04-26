97e215fccc0d260712028208db546d3c01928690
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-04-26 23:21:29 +0200

    added providers (event system)

---
+++ Runtime/Providers/EventSystemProviderInitializer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace WinuXGames.SplitFramework.Core.Providers
+{
+    public class EventSystemProviderInitializer : MonoBehaviour
+    {
+        [SerializeField] private EventSystem            _eventSystem;
+        [SerializeField] private SO_EventSystemProvider _eventSystemProvider;
+
+        private void Awake() { _eventSystemProvider.AssignEventSystem(_eventSystem); }
+    }
+}
---
+++ Runtime/Providers/SO_ScriptableProvider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace WinuXGames.SplitFramework.Core.Providers
+{
+    public abstract class SO_ScriptableProvider : ScriptableObject
+    {
+        protected abstract void ResetValues();
+        
+        private void OnEnable()
+        {
+            ResetValues();
+        }
+    }
+}
