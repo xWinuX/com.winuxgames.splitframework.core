5f1011f1c6f1d5e8d691df34f779714f5b00bf54
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-05-04 01:01:24 +0200

    Added general interfaces

---
+++ Runtime/IGameObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace WinuXGames.SplitFramework.Core
+{
+    public interface IGameObject
+    {
+        // ReSharper disable once InconsistentNaming
+        GameObject gameObject { get; }
+    }
+}
---
+++ Runtime/ITransform.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace WinuXGames.SplitFramework.Core
+{
+    public interface ITransform
+    {
+        // ReSharper disable once InconsistentNaming
+        Transform transform { get; }
+    }
+}
