071a2beea3ee999bc70454785685b70c5ec29387
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-05-06 21:15:15 +0200

    Added import presets and menuitem priority handler

---
+++ Editor/CustomGameObjectMenuItems.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+namespace WinuXGames.SplitFramework.Core.Editor
+{
+    public static class CustomGameObjectMenuItems
+    {
+        [MenuItem("GameObject/Split Framework", priority = 0)]
+        public static void SplitFramework() { }
+    }
+}
