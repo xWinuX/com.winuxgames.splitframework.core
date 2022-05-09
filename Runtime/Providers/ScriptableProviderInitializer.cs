fd4fa2a888d426ad7a9a1ea8f80b28703aaefb14
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-05-10 01:56:57 +0200

    Changed providers a bit

---
+++ Runtime/Providers/ScriptableProviderInitializer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace WinuXGames.SplitFramework.Core.Providers
+{
+    [DefaultExecutionOrder(-1)]
+    public abstract class ScriptableProviderInitializer<T> : MonoBehaviour where T : SO_ScriptableProvider
+    {
+        [SerializeField] private T _providerToInitialize;
+
+        protected T ProviderToInitialize => _providerToInitialize;
+    }
+}
