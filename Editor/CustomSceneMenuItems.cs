c00ef3512f19cc03835177fb6048fc783c80471a
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-05-11 18:18:41 +0200

    Added custom scene menu items

---
+++ Editor/CustomSceneMenuItems.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using SceneUtility = WinuXGames.SplitFramework.Utility.SceneUtility;
+using EditorUtility = WinuXGames.SplitFramework.Utility.Editor.EditorUtility;
+
+
+namespace WinuXGames.SplitFramework.Core.Editor
+{
+    public static class CustomSceneMenuItems
+    {
+        private static readonly List<Scene>  PreviousScenes     = new List<Scene>();
+        private static readonly List<string> PreviousScenePaths = new List<string>();
+        
+        [MenuItem("Custom/Scene/Load Current With Base")]
+        private static void LoadCurrentWithBase() { LoadCurrentWithBaseScene(); }
+
+        private static void LoadScenesEditor(IReadOnlyList<string> scenePaths, bool unloadOpen = true)
+        {
+            for (int i = 0; i < scenePaths.Count; i++)
+            {
+                string previousScene = scenePaths[i];
+                EditorSceneManager.OpenScene(previousScene, i == 0 && unloadOpen ? OpenSceneMode.Single : OpenSceneMode.Additive);
+            }
+        }
+
+        private static void LoadCurrentWithBaseScene()
+        {
+            if (!EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo()) { return; }
+
+            PreviousScenePaths.Clear();
+            SceneUtility.GetCurrentlyLoadedScenesNonAlloc(PreviousScenes);
+            PreviousScenePaths.AddRange(PreviousScenes.Select(x => x.path));
+            
+            EditorSceneManager.OpenScene(EditorUtility.BaseScenePath, OpenSceneMode.Single);
+            
+            LoadScenesEditor(PreviousScenePaths, false);
+        }
+    }
+}
